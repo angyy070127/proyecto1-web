0af9b5e16468ac08d2013d9893f4949a2970d721
angyy070127 <angelaaamartinez77@gmail.com>
2025-01-28 13:47:15 -0600

    Add files via upload
    se agrego carpeta JAVASCRIPT

---
+++ JAVASCRIPT/script.js
@@ -0,0 +1,6 @@
+//obtener el botn por su ID que le puse
+const boton=document.getElementById('botoncito');
+//agregar un evento al boton
+boton.addEventListener('click',function(){
+alert("hola ya le diste click");
+});
